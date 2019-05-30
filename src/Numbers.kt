e3bed6f2592c1c8a90484cffd5cd1766bee7b4fc
derysudrajat <dery.sudrajat17@gmail.com>
2019-05-31 00:42:43 +0700

    Add Numbers.kt, contain Number operations in kotlin

---
+++ src/Numbers.kt
@@ -0,0 +1,50 @@
+fun main(args: Array<String>) {
+    //Lesson 1
+    val maxInt = Int.MAX_VALUE
+    println(maxInt)
+
+    //Lesson 2
+    val number1 = 13
+    val number2 = 9
+    println(number1%number2)
+
+    //Lesson 3
+    val number3 = 5
+    val number4 = 2
+    println(number3/number4)
+
+    //Lesson 4
+    println(5+7*10)
+
+    //Lesson 5
+    println((5+7)*10)
+
+    //Lesson 6
+    val number5 = 5
+    val number6 = 7L
+    val total = number5+number6
+    println(total)
+
+    //Lesson 7
+    val num1 = 5
+    val num2 =2.0
+    val tot = num1/num2
+    println(tot)
+
+    //Lesson 8
+    val intNum = 3
+    val dounleNum = intNum.toDouble()
+    println(dounleNum)
+
+    //Lesson 9
+    val StringNum = "333"
+    val intNumers = StringNum.toInt()
+    println(intNumers)
+
+    //Lesson 10
+    val intNumme = 1999
+    val born = "I was Born on "+intNumme.toString()
+    println(born)
+
+
+}
